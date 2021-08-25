30985a5daa74a596ab89197618865dcc82f3f676
Andrey Arzumaniyan <88486829+AndreyArzumaniyan@users.noreply.github.com>
2021-08-25 17:55:59 +0600

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  const swiper = new Swiper('.swiper-container', {
+  const element = document.querySelector('.gallery__select');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    itemSelectText: 'ВЫбрать',
+  });
+
+  new Swiper('.hero__slider-container', {
     loop: true,
     autoplay: {
       delay: 2000,
@@ -19,6 +25,22 @@ document.addEventListener('DOMContentLoaded', function () {
     effect: 'fade',
     fadeEffect: {
       crossFade: true,
+    },
+  });
+
+  new Swiper('.gallery__slider-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerColumn: 2,
+    slidesToScroll: 1,
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: '.gallery__slider-button-next',
+      prevEl: '.gallery__slider-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
     },
   });
 });
