64767f39a47166360355aae382ad9f3dbe526d5c
Andrey Arzumaniyan <88486829+AndreyArzumaniyan@users.noreply.github.com>
2021-08-26 00:34:03 +0600

    commit

--- app/js/vendor.js
+++ app/js/vendor.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
+  $('.catalog__accordion-title--active').toggleClass('active').next().slideDown(300);
   $('.catalog__accordion-title').click(function (event) {
+    $('.catalog__accordion-title').removeClass('active');
     if ($('.catalog__accordion').hasClass('only-one')) {
       $('.catalog__accordion-title').not($(this)).removeClass('active');
       $('.catalog__accordion-wrapper').not($(this).next()).slideUp('active');
     }
-    $(this).toggleClass('active').next().slideToggle(300);
+    $(this).toggleClass('active').next().slideDown(300);
+  });
+
+  $('.events__btn').click(function (event) {
+    $('.events__inner').toggleClass('active');
   });
 });
