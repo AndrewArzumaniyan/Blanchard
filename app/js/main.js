24e65ee69368f2d924f950b78a9dc7c8c48db6ee
Andrey Arzumaniyan <88486829+AndreyArzumaniyan@users.noreply.github.com>
2021-08-27 02:09:42 +0600

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -12,14 +12,54 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   });
 
-  const elements = document.querySelectorAll('.header__select');
-  elements.forEach(function (element) {
-    const choices = new Choices(element, {
-      searchEnabled: false,
-      placeholder: true,
-      itemSelectText: 'Выбрать',
+  //!Header__under
+  const params = {
+    btnClassName: "main__item-btn",
+    activeClassName: "is-active",
+    disabledClassName: "is-disabled"
+  }
+
+  function onDisable(evt) {
+    if (evt.target.classList.contains(params.disabledClassName)) {
+      evt.target.classList.remove(params.disabledClassName, params.activeClassName);
+      evt.target.removeEventListener("animationend", onDisable);
+    }
+  }
+
+  function setMenuListener() {
+    document.body.addEventListener("click", (evt) => {
+      const activeElements = document.querySelectorAll(`.${params.activeClassName}`);
+
+      if (activeElements.length && !evt.target.closest(`.${params.activeClassName}`)) {
+        activeElements.forEach((current) => {
+          if (current.classList.contains(params.btnClassName)) {
+            current.classList.remove(params.activeClassName);
+          } else {
+            current.classList.add(params.disabledClassName);
+          }
+        });
+      }
+
+      if (evt.target.closest(`.${params.btnClassName}`)) {
+        const btn = evt.target.closest(`.${params.btnClassName}`);
+        const path = btn.dataset.path;
+        const drop = document.querySelector(`[data-target="${path}"]`);
+
+        btn.classList.toggle(params.activeClassName);
+
+        if (!drop.classList.contains(params.activeClassName)) {
+          drop.classList.add(params.activeClassName);
+          drop.addEventListener("animationend", onDisable);
+        } else {
+          drop.classList.add(params.disabledClassName);
+        }
+      }
     });
-  });
+  }
+
+  setMenuListener();
+  //!
+
 
   const element = document.querySelector('.gallery__select');
   const choices = new Choices(element, {
